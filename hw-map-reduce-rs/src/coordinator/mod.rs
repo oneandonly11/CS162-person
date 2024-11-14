a996d05b01c98f190eab07ba36696b1936fa7f8d
谭佳明 <2247854466@qq.com>
2024-11-14 21:25:04 +0800

    finish Tasks

--- hw-map-reduce-rs/src/coordinator/mod.rs
+++ hw-map-reduce-rs/src/coordinator/mod.rs
@@ -20,8 +20,7 @@ pub mod args;
 
 pub enum WorkerStatus{
     Idle,
-    Mapping,
-    Reducing,
+    Doing,
     GG,
 }
 
@@ -57,14 +56,22 @@ pub struct Job{
     pub output_dir: String,
     pub args: Vec<u8>,
     pub status: JobStatus,
+    pub is_full: bool,
     pub map_tasks: Vec<MapTask>,
     pub reduce_tasks: Vec<ReduceTask>,
+}
+
+pub struct WorkerTask{
+    pub job_id: JobId,
+    pub task_id: TaskNumber,
+    pub reduce: bool,
 }
 
 pub struct Worker {
     pub id: WorkerId,
     pub last_heartbeat: Instant,
     pub status: WorkerStatus,
+    pub task: WorkerTask,
 }
 pub struct CoordinatorData {
     pub workers: HashMap<WorkerId, Worker>,
@@ -76,16 +83,31 @@ pub struct CoordinatorData {
 
 impl Job {
     pub fn new(req: SubmitJobRequest ) -> Self {
-        let job = Job {
+        let mut job = Job {
             files: req.files.clone(),
             n_reduce: req.n_reduce as usize,
             app: req.app.clone(),
             output_dir: req.output_dir.clone(),
             args: req.args.clone(),
             status: JobStatus::Waiting,
+            is_full: false,
             map_tasks: Vec::new(),
             reduce_tasks: Vec::new(),
         };
+        job.map_tasks = (0..job.files.len() as TaskNumber).map(|task_id| {
+            MapTask {
+                task_id,
+                worker_id: 0,
+                status: TaskStatus::Waiting,
+            }
+        }).collect();
+        job.reduce_tasks = (0..job.n_reduce as TaskNumber).map(|task_id| {
+            ReduceTask {
+                task_id,
+                worker_id: 0,
+                status: TaskStatus::Waiting,
+            }
+        }).collect();
         job
         
     }
@@ -185,6 +207,12 @@ impl coordinator_server::Coordinator for Coordinator {
             id: worker_id,
             last_heartbeat: Instant::now(),
             status: WorkerStatus::Idle,
+            task: WorkerTask {
+                job_id: 0,
+                task_id: 0,
+                reduce: false,
+            },
+            
         });
         log::info!("Registered worker {}", worker_id);
         Ok(Response::new(RegisterReply { worker_id }))
@@ -195,7 +223,7 @@ impl coordinator_server::Coordinator for Coordinator {
         req: Request<GetTaskRequest>,
     ) -> Result<Response<GetTaskReply>, Status> {
         // TODO: Tasks
-        Ok(Response::new(GetTaskReply {
+        let mut task =  GetTaskReply {
             job_id: 0,
             output_dir: "".to_string(),
             app: "".to_string(),
@@ -207,7 +235,67 @@ impl coordinator_server::Coordinator for Coordinator {
             wait: true,
             map_task_assignments: Vec::new(),
             args: Vec::new(),
-        }))
+        };
+
+        let mut data = self.data.lock().await;
+        let worker_id = req.get_ref().worker_id;
+
+        let job_queue = data.job_queue.clone();
+        
+        let job_id = match job_queue.iter().find(|job_id| {
+            matches!(data.jobs.get_mut(job_id).unwrap().is_full, false)
+        }){
+            Some(&job_id) => job_id,
+            None => return Ok(Response::new(task)),
+        };
+        
+        let job = data.jobs.get_mut(&job_id).unwrap();
+        task.job_id = job_id;
+        task.app = job.app.clone();
+        task.output_dir = job.output_dir.clone();
+        task.args = job.args.clone();
+        task.n_reduce = job.n_reduce as u32;
+        task.n_map = job.files.len() as u32;
+
+        match job.status {
+            JobStatus::Mapping => {
+                let map_task = job.map_tasks.iter_mut().find(|map_task| {
+                    matches!(map_task.status, TaskStatus::Waiting)
+                }).unwrap();
+                map_task.status = TaskStatus::Running;
+                task.task = map_task.task_id as u32;
+                task.file = job.files[map_task.task_id as usize].clone();
+                if map_task.task_id == job.files.len() as TaskNumber - 1 {
+                    job.is_full = true;
+                }
+                task.reduce = false;
+                task.wait = false;
+            }
+            JobStatus::Reducing => {
+                let reduce_task = job.reduce_tasks.iter_mut().find(|reduce_task| {
+                    matches!(reduce_task.status, TaskStatus::Waiting)
+                }).unwrap();
+                reduce_task.status = TaskStatus::Running;
+                task.task = reduce_task.task_id as u32;
+                if reduce_task.task_id == job.n_reduce as TaskNumber - 1 {
+                    job.is_full = true;
+                }
+                task.reduce = true;
+                task.wait = false;
+            }
+            _ => return Ok(Response::new(task)),
+        }
+        if matches!(job.status, JobStatus::Waiting) {
+            job.status = JobStatus::Mapping;
+        }
+        let worker = data.workers.get_mut(&worker_id).unwrap();
+        worker.task = WorkerTask {
+            job_id: job_id,
+            task_id: task.task as usize,
+            reduce: task.reduce,
+        };
+        worker.status = WorkerStatus::Doing;
+        Ok(Response::new(task))
     }
 
     async fn finish_task(
@@ -215,6 +303,30 @@ impl coordinator_server::Coordinator for Coordinator {
         req: Request<FinishTaskRequest>,
     ) -> Result<Response<FinishTaskReply>, Status> {
         // TODO: Tasks
+        let req = req.into_inner();
+        let mut data = self.data.lock().await;
+        let worker_id = req.worker_id;
+        let job_id = req.job_id;
+        let job = data.jobs.get_mut(&job_id).unwrap();
+        if req.reduce {
+            let reduce_task = job.reduce_tasks.iter_mut().find(|reduce_task| {
+                reduce_task.task_id == req.task as usize
+            }).unwrap();
+            reduce_task.status = TaskStatus::Finished;
+            if job.reduce_tasks.iter().all(|reduce_task| matches!(reduce_task.status, TaskStatus::Finished)) {
+                job.status = JobStatus::Finished;
+                data.job_queue.pop_front();
+            }
+        } else {
+            let map_task = job.map_tasks.iter_mut().find(|map_task| {
+                map_task.task_id == req.task as usize
+            }).unwrap();
+            map_task.status = TaskStatus::Finished;
+            if job.map_tasks.iter().all(|map_task| matches!(map_task.status , TaskStatus::Finished)) {
+                job.status = JobStatus::Reducing;
+                job.is_full = false;
+            }
+        }
         Ok(Response::new(FinishTaskReply {}))
     }
 
