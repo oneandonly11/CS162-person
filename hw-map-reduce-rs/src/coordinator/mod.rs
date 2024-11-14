26486bca7916ff0c6fcce06702541f27ae4d19e8
谭佳明 <2247854466@qq.com>
2024-11-14 19:34:11 +0800

    finish Job submission

--- hw-map-reduce-rs/src/coordinator/mod.rs
+++ hw-map-reduce-rs/src/coordinator/mod.rs
@@ -4,25 +4,91 @@
 use anyhow::Result;
 use tonic::transport::Server;
 use tonic::{Request, Response, Status};
+use tonic::Code;
 use tokio::sync::Mutex;
 use tokio::time::Instant;
-use std::collections::HashMap;
+use std::collections::{HashMap, VecDeque};
 use std::sync::Arc;
 
 use crate::rpc::coordinator::*;
 use crate::*;
 use crate::log;
+use crate::app::named;
 
 pub mod args;
 
 
+pub enum WorkerStatus{
+    Idle,
+    Mapping,
+    Reducing,
+    GG,
+}
+
+pub enum TaskStatus{
+    Waiting,
+    Running,
+    Finished,
+}
+
+pub enum JobStatus{
+    Waiting,
+    Mapping,
+    Reducing,
+    Finished,
+}
+
+pub struct MapTask{
+    pub task_id: TaskNumber,
+    pub worker_id: WorkerId,
+    pub status: TaskStatus,
+}
+
+pub struct ReduceTask{
+    pub task_id: TaskNumber,
+    pub worker_id: WorkerId,
+    pub status: TaskStatus,
+}
+
+pub struct Job{
+    pub files: Vec<String>,
+    pub n_reduce: usize,
+    pub app: String,
+    pub output_dir: String,
+    pub args: Vec<u8>,
+    pub status: JobStatus,
+    pub map_tasks: Vec<MapTask>,
+    pub reduce_tasks: Vec<ReduceTask>,
+}
+
 pub struct Worker {
     pub id: WorkerId,
     pub last_heartbeat: Instant,
+    pub status: WorkerStatus,
 }
 pub struct CoordinatorData {
     pub workers: HashMap<WorkerId, Worker>,
     pub next_worker_id: WorkerId,
+    pub jobs: HashMap<JobId,Job>,
+    pub next_job_id: JobId,
+    pub job_queue: VecDeque<JobId>,
+}
+
+impl Job {
+    pub fn new(req: SubmitJobRequest ) -> Self {
+        let job = Job {
+            files: req.files.clone(),
+            n_reduce: req.n_reduce as usize,
+            app: req.app.clone(),
+            output_dir: req.output_dir.clone(),
+            args: req.args.clone(),
+            status: JobStatus::Waiting,
+            map_tasks: Vec::new(),
+            reduce_tasks: Vec::new(),
+        };
+        job
+        
+    }
 }
 
 pub struct Coordinator {
@@ -35,6 +101,9 @@ impl Coordinator {
         Self { data: Arc::new(Mutex::new(CoordinatorData {
             workers: HashMap::new(),
             next_worker_id: INITIAL_WORKER_ID,
+            jobs: HashMap::new(),
+            next_job_id: INITIAL_JOB_ID,
+            job_queue: VecDeque::new(),
         })) }
     }
 }
@@ -58,14 +127,33 @@ impl coordinator_server::Coordinator for Coordinator {
         &self,
         req: Request<SubmitJobRequest>,
     ) -> Result<Response<SubmitJobReply>, Status> {
-        todo!("Job submission")
+        // todo!("Job submission")
+        let req = req.into_inner();
+        let mut data = self.data.lock().await;
+        let job_id = data.next_job_id;
+        data.next_job_id += 1;
+        named(&req.app).map_err(|e| Status::new(Code::InvalidArgument, e.to_string()))?;  
+        
+        let job = Job::new(req);
+        data.jobs.insert(job_id, job);
+        data.job_queue.push_back(job_id);
+        log::info!("Job {} submitted", job_id);
+        Ok(Response::new(SubmitJobReply { job_id }))
     }
 
     async fn poll_job(
         &self,
         req: Request<PollJobRequest>,
     ) -> Result<Response<PollJobReply>, Status> {
-        todo!("Job submission")
+        // todo!("Job submission")
+        let mut data = self.data.lock().await;
+        let job_id = req.get_ref().job_id;
+        let job = data.jobs.get_mut(&job_id).ok_or(Status::new(Code::NotFound, "job id is invalid"))?;
+        Ok(Response::new(PollJobReply { 
+            done: false ,
+            failed: false,
+            errors: Vec::new(),
+        }))
     }
 
     async fn heartbeat(
@@ -96,6 +184,7 @@ impl coordinator_server::Coordinator for Coordinator {
         data.workers.insert(worker_id, Worker {
             id: worker_id,
             last_heartbeat: Instant::now(),
+            status: WorkerStatus::Idle,
         });
         log::info!("Registered worker {}", worker_id);
         Ok(Response::new(RegisterReply { worker_id }))
